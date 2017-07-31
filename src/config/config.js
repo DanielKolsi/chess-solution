8c8b4ed6c7c710e1910b8f9edbab72fd9255c53f
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-07-31 10:41:16 -0500

    initial position change

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-  setup: [
+  /*setup: [
     [0, 'RookBA', 'bra', 0, false, -5], //location, name, id, n (number-id), white (true/false), value
     [1, 'KnightBB', 'bka', 1, false, -3],
     [2, 'BishopBC', 'bbc', 2, false, -3.2],
@@ -22,6 +22,26 @@ export default {
     [51, 'PawnWD','wpd', 51, true, 1],
     [52, 'PawnWE','wpe', 52, true, 1],
     [53, 'PawnWF','wpf', 53, true, 1],
+    [54, 'PawnWG','wpg', 54, true, 1],
+    [55, 'PawnWH','wph', 55, true, 1],
+    [56, 'RookWA','wra', 56, true, 5],
+    [57, 'KnightWB','wkb', 57, true, 3],
+    [58, 'BishopWC','wbc', 58, true, 3.2],
+    [59, 'QueenW','wq', 59, true, 9],
+    [60, 'KingW','wk', 60, true, 4],
+    [61, 'BishopWF','wbf', 61, true, 3.2],
+    [62, 'KnightWG','wkg', 62, true, 3],
+    [63, 'RookWH','wrh', 63, true, 5]
+
+  ]*/
+
+  setup: [
+  /*  [48, 'PawnWA', 'wpa', 48, true, 1],
+    [49, 'PawnWB','wpb', 49, true, 1],
+    [26, 'PawnWC','wpc', 50, true, 1],
+    [51, 'PawnWD','wpd', 51, true, 1],
+    [52, 'PawnWE','wpe', 52, true, 1],
+    [53, 'PawnWF','wpf', 53, true, 1],*/
     [54, 'PawnWG','wpg', 54, true, 1],
     [55, 'PawnWH','wph', 55, true, 1],
     [56, 'RookWA','wra', 56, true, 5],
