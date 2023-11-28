a0f5be871d5728f1e8298fa2e886af6b811c4cea
Daniel Kolsi <kolsi.daniel@gmail.com>
2023-11-28 13:20:41 -0600

    eslintrc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 //import config from './config/normal'; // normal chess board setup
 //import config from './config/whiteCastling'; // castling
-//import config from './config/whiteCanCaptureWithEnPassant'; 
+//import config from './config/whiteCanCaptureWithEnPassant';
 //import config from "./config/staleMate";
 //import config from "./config/bugWhiteBishopCapturesKing";
 
 //import config from "./config/bugWhiteKingMovesToCheckByBlackBishop"; // fixed 27.10.21
 //import config from "./config/generalBugFixing";
-import config from "./config/549"; 
-//import config from "./config/60"; 
-//mport config from "./config/threatScoresDebug"; 
+import config from "./config/549";
+//import config from "./config/60";
+//mport config from "./config/threatScoresDebug";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -17,8 +17,5 @@ import registerServiceWorker from "./registerServiceWorker";
 import "./styles/Chess.css";
 //require('./styles/chess.scss'); // does the squares!!
 
-ReactDOM.render(
-  <App chess={config.pieces} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App chess={config.pieces} />, document.getElementById("root"));
 registerServiceWorker();
